5b380ac3a0f9c778997c8be63cf1d0861c1aaa64
jesper boer <jespercboer1@gmail.com>
2026-04-17 23:26:18 +0200

    updated logic beghind the game options, now works for 100+ options in the future, got rid of the turnSystem completely, added logs in assests folder

---
+++ js/data/actionData.js
@@ -0,0 +1,11 @@
+export const actionData = {
+    repairCar: {
+        money: 120,
+        reputation: 1,
+        flags: { repairedFirstCar: true }
+    },
+    
+    buyParts: {
+        money: -100
+    }
+}
--- js/ui/buttons.js
+++ js/ui/buttons.js
@@ -1,14 +1,16 @@
 import { SCENES } from "../data/scenes.js";
 import { gameState } from "../core/state.js";
 import { goToScene } from "../core/sceneManager.js";
-import * as systems from "../systems/actions.js";
+import {performAction} from "../systems/actions.js";
 
 export function handleOption(index) {
   const scene = SCENES[gameState.currentScene];
   const option = scene.options[index];
 
   if (option.action) {
-    systems[option.action]();
+    console.log("Performing action:", option.action);
+    performAction(option.action);
+    console.log("Performed action:", option.action);
   }
 
   if (option.goto) {
